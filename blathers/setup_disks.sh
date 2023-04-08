bfa4f60b87fa9adbcb0f78bad47bb6a3c48f623d
LizeLive <git@lize.live>
2023-04-08 11:41:00 -0700

    disk parts where wrong

--- blathers/setup_disks.sh
+++ blathers/setup_disks.sh
@@ -6,7 +6,9 @@ boot_device='/dev/disk/by-path/pci-0000:04:00.3-usb-0:3:1.0-scsi-0:0:0:0'
 mkfs.btrfs --csum xxhash --data raid0 --metadata raid0 --label blathers-nvmes /dev/nvme1n1
 
 # setup boot device
-# printf "label: gpt\n,550M,U\n,,L\n" | sfdisk boot_device
+printf "label: gpt\n,550M,U\n,,L\n" | sfdisk $boot_device
+mkfs.fat -F 32 "$boot_device-part1"
+mount "$boot_device-part1" /boot
 
 mount /dev/disk/by-label/blathers-nvmes /mnt
 btrfs subvolume create /mnt/root
