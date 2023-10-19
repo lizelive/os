ec4d9f8dbf27337c49b3c738ec863731e6f5fd2e
lize.live <git@lize.live>
2023-10-19 15:26:40 -0700

    add obs

--- graphical-tools.nix
+++ graphical-tools.nix
@@ -4,6 +4,7 @@
     microsoft-edge # todo: figure out how to only install on ui
     logseq # notes
     zotero # more notes!
-    blender # crime
+    blender # 3d modeling and video editing
+    obs-studio # streaming
   ];
 }
