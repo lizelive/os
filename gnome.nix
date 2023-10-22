13d51c9c5c9e80c3e9494567c29e9b9ab2046781
lize.live <git@lize.live>
2023-10-22 04:12:07 -0700

    remove gnome.file-roller

--- gnome.nix
+++ gnome.nix
@@ -1,5 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    file-roller
+  ];
 }
