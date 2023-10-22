4acaef6bbb5c56c4c49fe104b6f480979bdcc3ca
lize.live <git@lize.live>
2023-10-22 03:51:21 -0700

    fmt

--- wayland.nix
+++ wayland.nix
@@ -1,7 +1,7 @@
 {...}: {
-    sound.enable = true;
-    nixpkgs.config.pulseaudio = true;
-    hardware.pulseaudio.enable = true;
-    services.xserver.enable = true;
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  sound.enable = true;
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = true;
+  services.xserver.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
