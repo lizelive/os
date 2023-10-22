00df1815d0d6d7d59710bda779cf234c4b44b43d
lizelive <github@lize.live>
2023-10-21 22:41:47 -0700

    wacomtablet

--- kde.nix
+++ kde.nix
@@ -1,4 +1,7 @@
-{...}: {
+{pkgs, ...}: {
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  environment.systemPackages = with pkgs; [
+    wacomtablet
+  ];
 }
