67f43bb4c43c355ade669a7d349d952d23918d25
lize.live <git@lize.live>
2023-10-22 04:21:37 -0700

    gnome failed to start

--- gnome.nix
+++ gnome.nix
@@ -2,7 +2,5 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = with pkgs.gnome; [
-    file-roller
-  ];
+  # environment.gnome.excludePackages = with pkgs.gnome; [ file-roller ];
 }
