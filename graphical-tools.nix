e595eb2e69fba31e69383a8ecc9bb910ab9ab5d4
lize.live <git@lize.live>
2023-10-23 14:56:01 -0700

    add vscode extensions

--- graphical-tools.nix
+++ graphical-tools.nix
@@ -2,7 +2,20 @@
   # programs
   environment.systemPackages = with pkgs; [
     firefox
-    vscodium
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        vadimcn.vscode-lldb
+        tamasfe.even-better-toml
+        serayuzgur.crates
+        rust-lang.rust-analyzer
+        ms-python.python
+        mkhl.direnv
+        jnoortheen.nix-ide
+        github.copilot-chat
+        github.copilot
+      ];
+    })
     logseq # notes
     zotero # more notes!
     blender # 3d modeling and video editing
