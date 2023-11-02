f47d1cbf3e122b8cbe9ed013271e0c66880001fc
lizelive <git@lize.live>
2023-11-01 23:51:27 -0700

    add tpm key

--- lizelive.nix
+++ lizelive.nix
@@ -12,7 +12,10 @@
     extraGroups = [ "wheel" "podman" "docker" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
     ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOU1vPhNxJbXF2Gaq40kbKQ7bt7darBTNCTqDPq180yo" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOU1vPhNxJbXF2Gaq40kbKQ7bt7darBTNCTqDPq180yo"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBP9hCD7L1DYXsiqJi9KeUynxHssILCWO4kzHMPoKvXGKnmfCuNEBrQFO/yxdIdQhEe4JHF7aWZgrZ3o339iz8YE="
+      ];
 
     # openssh.authorizedKeys.keys = keys;
   };
