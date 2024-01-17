995cf05688971e44d3dc89b0fef91c0f974f32eb
lizelive <git@lize.live>
2024-01-16 21:36:15 -0800

    remove tpm

--- reese/configuration.nix
+++ reese/configuration.nix
@@ -40,10 +40,5 @@
   #   use = "if, usev6=ifv6, ifv6=eno1, usev4=disabled";
   # };
 
-  security.tpm2.enable = true;
-  security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
-  security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
-  users.users.lizelive.extraGroups = [ "tss" ];  # tss group has access to TPM devices
-
   services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
 }
